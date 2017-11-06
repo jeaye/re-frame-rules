e6ef6c30702853469bd84c7db6a5e43c724fda71
jeaye <contact@jeaye.com>
2017-11-06 11:10:22 -0800

    Add initial project

---
+++ project.clj
@@ -0,0 +1,11 @@
+(defproject re-frame-rules "0.1.0"
+  :description ""
+  :url "https://github.com/jeaye/re-frame-rules"
+  :license {:name "jank license"
+            :url "https://upload.jeaye.com/jank-license"}
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
+                 [org.clojure/clojurescript "1.9.946"]
+                 [re-frame "0.10.1"]]
+
+  :source-paths ["src"]
+  :test-paths ["test"])
